71f88bcb75d98a2b3fc519649d03bf2fc4a8d224
realBarry123 <barryyu28@gmail.com>
2025-01-27 19:23:38 -0800

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import { useState, useEffect } from "react";
 import "./App.css";
 import io from 'socket.io-client';
 const socket = io('http://localhost:4000');
 
 function App() {
 
-  const [input, setInput] = useState("");
-
-  // Send a message to the server
-  const sendMessage = (msg) => {
-    socket.emit("message", msg);
-    console.log(msg);
-  };
-
-  socket.on("message", (msg) => {
-    setInput(msg);
-    console.log(msg);
-  })
-
-  useEffect(() => {
-
-    sendMessage(input);
-
-    //return () => socket.disconnect();
-  }, [input]);
-
   return (
     <div className="App">
-      <h2>{input}</h2>
-      <textarea 
-	      required
-	      value={input}
-	      onChange={(e) => setInput(e.target.value)}
-	    />
     </div>
   );
   
