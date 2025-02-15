3ff5327124b92b5934f2f27c79e9bf0c6ff82a62
realBarry123 <barryyu28@gmail.com>
2025-02-15 11:16:33 -0800

    Redo some of the join thing

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,27 +39,33 @@ const Home = ({ socket }) => {
 
     return (
         <div className="home">
-            <h1>ONDES</h1>
+            <h1>Ondes</h1>
             <h3>Created by Barry Yu</h3>
-            <button 
-                onClick={toHost} 
-                style={{borderRight: "0.5px solid #ffff", width: "120px"}}
-            >Create New</button>
             
-            <button 
-                onClick={() => {setJoinOn(!joinOn)}} 
-                style={{borderLeft: "0.5px solid #ffff", width: "120px"}}
-            >Join Ensemble</button>
+            {!joinOn && <div>
+                <button 
+                    onClick={toHost} 
+                    style={{borderRight: "0.5px solid #ffff", width: "120px"}}
+                >create new</button>
+                
+                <button 
+                    onClick={() => {setJoinOn(!joinOn)}} 
+                    style={{borderLeft: "0.5px solid #ffff", width: "120px"}}
+                >join ensemble</button>
+            </div>}
 
             {joinOn && <div>
                 <input 
                     ref={setInputRef}
                     type="text" 
                     value={codeInput}
-                    placeholder="Join Code"
+                    placeholder="join code"
                     onChange={(e) => setCodeInput(e.target.value)}
                 />
-                <button onClick={() => {submitJoin(codeInput)}}>Submit</button>
+                <button 
+                    onClick={() => {submitJoin(codeInput)}}
+                    style={{marginLeft: "10px"}}
+                >submit</button>
             </div>}
 
 
