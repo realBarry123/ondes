fbbfdabb6d2aa732063aa93721974f5cbeb0c393
realBarry123 <barryyu28@gmail.com>
2025-03-13 19:49:39 -0700

    First instrument frontend works

--- src/components/instruments/Lith.js
+++ src/components/instruments/Lith.js
@@ -2,8 +2,11 @@ import { Instrument } from "../../Instrument.js";
 import * as Tone from "tone";
 
 const Lith = () => {
-    const notes = ["A#4", "C5", "D#5", "F#5", "A5", "A#5", "C6", "D#6", "E6", "F#6"];
+    
+    const notes = ["A4", "A#4", "C5", "D#5", "F#5", "A5", "A#5", "C6", "D#6", "E6", "F#6"];
     const limiter = new Tone.Limiter(-60).toDestination();
+
+    const isTouchDevice = "ontouchstart" in window;
     
     const synth = new Tone.PolySynth(Tone.Synth, {
         volume: -40,
@@ -12,12 +15,17 @@ const Lith = () => {
             partials: [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0.01]
         },
         envelope: {
-            attack: 0.5,
-            decay: 0.1,
-            sustain: 0.3,
-            release: 1
+            attack: 0.01,
+            decay: 0.01,
+            sustain: 0.5,
+            release: 4
         }
     })
+
+    const handlePlay = (note, e) => {
+        e.preventDefault();
+        instrument.play(note, "8n");
+    }
 
     synth.connect(limiter);
 
@@ -25,13 +33,17 @@ const Lith = () => {
 
     return (
         <div className="lith">
-            {notes.map(note => (
-                <button 
-                    className="ui-button" 
-                    onClick={() => {instrument.play(note, "8n")}}
-                    key={note}
-                >{note}</button>
-            ))}
+            <div className="key-container">
+                {notes.map(note => (
+                    <button 
+                        className="key-button" 
+                        onMouseDown={!isTouchDevice ? (e) => {handlePlay(note, e)} : undefined}
+                        onTouchStart={isTouchDevice ? (e) => {handlePlay(note, e)} : undefined}
+                        key={note}
+                        style={{height: "200px", width:"50px"}}
+                    >{note}</button>
+                ))}
+            </div>
         </div>
     );
 }
